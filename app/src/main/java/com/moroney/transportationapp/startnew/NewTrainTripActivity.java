883a7465daef146af1601680b629b40bf828594d
Awwal M <67423428+awwalm@users.noreply.github.com>
2022-12-26 19:16:11 +0100

    Added new trip container modules

---
+++ app/src/main/java/com/moroney/transportationapp/startnew/NewTrainTripActivity.java
@@ -0,0 +1,18 @@
+package com.moroney.transportationapp.startnew;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.moroney.transportationapp.R;
+
+public class NewTrainTripActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_train_trip);
+    }
+}
